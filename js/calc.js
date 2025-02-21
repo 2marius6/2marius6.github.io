ee13616ebcc0435dd55e6a7ebb54823fc321ac03
2marius6 <mariusnichiteanu26@gmail.com>
2025-02-21 14:15:50 +0200

    update semestrul 2 (incl dizertatie)

--- js/calc.js
+++ js/calc.js
@@ -4,28 +4,30 @@ function getWeekNumber() {
     date.setDate(date.getDate() + 4 - (date.getDay() || 7));
     var yearStart = new Date(date.getFullYear(), 0, 1);
     var weekNo = Math.ceil(((date - yearStart) / 86400000 + 1) / 7);
-    if(weekNo >= 40 && weekNo <= 51){
-        weekNo = weekNo - 39;
+    if (weekNo == 8){
+        document.getElementById("date").innerHTML = "Sesiunea B1";
+    }
+    if(weekNo >= 9 && weekNo <= 16){
+        weekNo = weekNo - 8;
         document.getElementById("date").innerHTML = weekNo;
         if(weekNo%2!==0){
             document.getElementById("oddEven").innerHTML = "impară";
         }else{
             document.getElementById("oddEven").innerHTML = "pară";
         }
-    } else if (weekNo >= 2 && weekNo <= 3){
-        weekNo = weekNo + 11;
+    } else if (weekNo >= 18 && weekNo <= 23){
+        weekNo = weekNo - 9;
         document.getElementById("date").innerHTML = weekNo;
         if(weekNo%2!==0){
             document.getElementById("oddEven").innerHTML = "impară";
         }else{
             document.getElementById("oddEven").innerHTML = "pară";
         }
-        console.log(weekNo);
-    } else if (weekNo >= 4 && weekNo <= 6){
-        document.getElementById("date").innerHTML = "Sesiunea A1";
-    } else if (weekNo == 8){
-        document.getElementById("date").innerHTML = "Sesiunea B1 (update pentru semestrul 2 curând)";
-    } else {
-        document.getElementById("date").innerHTML = "Vacanță";
+    }else if (weekNo >= 24 && weekNo <= 26){
+        document.getElementById("date").innerHTML = "Sesiunea A2";
+    } else if (weekNo == 27){
+        document.getElementById("date").innerHTML = "Sesiunea B2";
+    } else if (weekNo == 28){
+        document.getElementById("date").innerHTML = "Prezentare dizertație";
     }
 }
